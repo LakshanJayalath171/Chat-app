6c633d057462783f2396910758bbbef5abbbf6cc
Lakshan Jayalath <lakshanjayalath171@gmail.com>
2026-03-20 10:59:33 +0530

    Created sidebar component successfull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Hompage from './pages/Hompage'
+import LoginPage from './pages/LoginPage'
+import Profile from './pages/Profile'
+import bgImage from "./assets/bgImage.svg";
 
 const App = () => {
   return (
-    <div className='bg-amber-500'>App</div>
+    <div className="bg-contain" style={{ backgroundImage: `url(${bgImage})` }}>
+      <Routes>
+        <Route path='/' element={<Hompage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/components/ChatContaineer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const ChatContaineer = () => {
+  return (
+    <div>
+        chat containeer
+    </div>
+  )
+}
+
+export default ChatContaineer
